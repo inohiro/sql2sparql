3f4fb7bc7b27a6ec4c8dbc64a75d5938b5b2a80d
Hiroyuki Inoue <mammymax@gmail.com>
2013-11-18 18:08:21 +0900

    add rspec

---
+++ spec/converter_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'Converter' do
+
+  describe 'Converto' do
+    
+  end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'rspec'
+require 'rspec/autorun'
+require 'sql2sparql'
+
+RSpec.configure do |config|
+  include SQL2SPARQL
+end
+
