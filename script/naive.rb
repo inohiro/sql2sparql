b5afdc3a799f3442e7fbf02832972d2be38aa7ee
Hiroyuki Inoue <mammymax@gmail.com>
2013-11-21 17:26:20 +0900

    wrote a little

--- script/naive.rb
+++ script/naive.rb
@@ -18,7 +18,7 @@ def trans_from_closure( tables = [] ) #, is_distinct = false )
   triple_pattern = {}
   tables.each do |table|
     name = table.to_s
-    triple = Triple.new( '', name, '' )
+    triple = Triple.new( '?subject', name, '?object' )
     triple_pattern.store( name, triple )
   end
   triple_pattern
@@ -31,6 +31,19 @@ def trans_where_closure( join_condition, boolean_expression, triple_pattern, ran
     triple_pattern.each do |tp|
       where_cond += tp.to_s
     end
+  end
+
+  # JC denots a set of join conditions where a condition is o the form: Attr Op Attr }
+  # join_condition.sort_by { |rank| rank }
+
+  join_condition.each do |p|
+    jc_left_operand = p.left_operand
+    jc_right_operand = p.right_operand
+
+    tp1 = TP.get( jc_left_operand ) # Get the triple pattern for p.left_operand
+    tp2 =  TP.get( jc_right_operand ) # Get the triple pattern for p.right_operand
+    where_cond += tp1.subject + "" + tp1.predicate + "" tp1.object
+
   end
 
   where_cond
